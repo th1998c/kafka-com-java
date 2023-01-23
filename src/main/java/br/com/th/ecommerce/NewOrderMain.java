5bb8d09cb029985ec31c4f9d8223305296596bc5
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2023-01-23 14:50:59 -0300

    serializando com GSON

--- src/main/java/br/com/th/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/th/ecommerce/NewOrderMain.java
@@ -1,20 +1,25 @@
 package br.com.th.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try(var dispatcher = new KafkaDispatcher()){
-            for(int count = 0; count< 10; count++){
-                var key = UUID.randomUUID().toString();
-                var value = key +", 6485, 5000.0";
+        try(var orderDispatcher = new KafkaDispatcher<Order>()){
+            try(var dispatcher = new KafkaDispatcher<String>()){
+                for(int count = 0; count< 10; count++){
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = Math.random() * 5000 + 1;
+                    var order = new Order(userId, orderId, new BigDecimal(amount));
 
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Welcome! We are processing your order.";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var email = "Welcome! We are processing your order.";
+                    dispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
+            }
             }
         }
-    }
 }
